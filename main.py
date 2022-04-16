a3441da0799cf09f9f6ec083196cdef115627bc2
Gavan228 <64046017+Gavan228@users.noreply.github.com>
2022-04-15 17:23:55 -0700

    lowered framerate for testing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("TheMadDoctor","TheMadDoctor.mp4",.1,.3,10)
+    encoder.encode("TheMadDoctor","TheMadDoctor.mp4",.1,.1,10)
 
 if __name__== "__main__":
     main()
